c6879d2ebac88d2daba3fa44d784bec83f82af15
xuzebin <zebinxu7@gmail.com>
2016-12-05 16:51:45 -0500

    fix bug

--- edu/nyu/algorithms/dp/CutRod.java
+++ edu/nyu/algorithms/dp/CutRod.java
@@ -1,21 +1,24 @@
-package edu.nyu.algorithms.dp;
-
+/**
+ * Rod Cutting implementation in chapter 15 in the CLRS book.
+ */
 public class CutRod {
     public static void main(String[] args) {
-	CutRod cr = new CutRod();
-	int[] prices = { 1, 5, 8 };
-	int max = cr.recursive(prices, 3);
-	System.out.printf("max: %d%n", max);
+        CutRod cr = new CutRod();
+        int[] prices = { 1, 5, 8, 9 };
+        int max = cr.cutRodRecursive(prices, 4);
+        System.out.printf("max: %d%n", max);
     }
-    
-    public int recursive(int[] prices, int n) {
-	if (n == 0) {
-	    return 0;
-	}
-	int q = Integer.MIN_VALUE;
-	for (int i = 0; i < prices.length; ++i) {
-	    q = Math.max(q, prices[i] + recursive(prices, n - i));
-	}
-	return q;
+ 
+    //Recursive top-down implementation
+    public int cutRodRecursive(int[] prices, int n) {
+        if (n == 0) {
+            return 0;
+        }
+        int q = Integer.MIN_VALUE;
+        for (int i = 0; i < n; ++i) {
+            q = Math.max(q, prices[i] + cutRodRecursive(prices, n - i - 1));
+        }
+        return q;
     }
+
 }
