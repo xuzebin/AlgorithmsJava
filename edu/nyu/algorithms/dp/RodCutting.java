99c9bbc3ad7f6474b4a98efd440ba139dcd425e9
xuzebin <zebinxu7@gmail.com>
2016-12-08 16:26:38 -0500

    add method & rename

--- edu/nyu/algorithms/dp/RodCutting.java
+++ edu/nyu/algorithms/dp/RodCutting.java
@@ -1,36 +1,39 @@
 /**
  * Rod Cutting implementation in chapter 15 in the CLRS book.
  * It includes:
- * Naive implementation using recursion O(2^n)
- * DP solution using top-down approach: memoize recursive algorithm
- * DP solution using bottom-up approach: solve the subproblems in size order (smallest subproblems first).
- * 
+ * cutRod: Naive implementation using recursion. Time O(2^n).
+ * memoizedCutRod: DP solution using top-down approach: memoize recursive algorithm. Time O(n^2).
+ * bottomUpCutRod: DP solution using bottom-up approach: solve the subproblems in size order (smallest subproblems first). Time: O(n^2).
+ * printCutRodSolution: prints the optimal solution with size of cuts.
  */
-public class CutRod {
+public class RodCutting {
+
     public static void main(String[] args) {
-        CutRod cr = new CutRod();
+        RodCutting rc = new RodCutting();
         int[] prices = { 1, 5, 8, 9, 10, 17, 17, 20, 24, 30 };
 
         for (int i = 0; i < prices.length + 1; i++) {
-            int max0 = cr.cutRodRecursive(prices, i);
-            int max1 = cr.memoizedCutRod(prices, i);
-            int max2 = cr.bottomUpCutRod(prices, i);
+            int max0 = rc.cutRod(prices, i);
+            int max1 = rc.memoizedCutRod(prices, i);
+            int max2 = rc.bottomUpCutRod(prices, i);
             System.out.printf("recursive max: %d%n", max0);
             System.out.printf("memoized max: %d%n", max1);
             System.out.printf("bottomUp (dp) max: %d%n", max2);
+            rc.printCutRodSolution(prices, i);
             System.out.println("----------------");
         }
+
     }
  
     //Naive version: recursive top-down implementation
     //Time: O(2^n)
-    public int cutRodRecursive(int[] prices, int n) {
+    public int cutRod(int[] prices, int n) {
         if (n == 0) {
             return 0;
         }
         int max = Integer.MIN_VALUE;
         for (int i = 0; i < n; ++i) {
-            max = Math.max(max, prices[i] + cutRodRecursive(prices, n - i - 1));
+            max = Math.max(max, prices[i] + cutRod(prices, n - i - 1));
         }
         return max;
     }
@@ -65,9 +68,7 @@ public class CutRod {
     //Time: O(n^2)
     public int bottomUpCutRod(int[] prices, int n) {
         int[] revs = new int[n + 1];
-        for (int i = 0; i < revs.length; i++) {
-            revs[i] = 0;//no harm to initialize all revs instead of only revs[0] to 0.
-        }
+        revs[0] = 0;//the revenue of a rod of length 0 is 0.
         int max = Integer.MIN_VALUE;
         for (int j = 1; j <= n; j++) {//revs[j] indicates maximum revenue of a rod of length j.
             max = Integer.MIN_VALUE;
@@ -78,5 +79,44 @@ public class CutRod {
             revs[j] = max;
         }
         return revs[n];
+    }
+
+
+    public int[][] extendedBottomUpCutRod(int[] prices, int n) {
+        int[] revs = new int[n + 1];
+        int[] size = new int[n + 1];
+        
+        revs[0] = 0;
+        int max = Integer.MIN_VALUE;
+        for (int j = 1; j <= n; j++) {
+            max = Integer.MIN_VALUE;
+            for (int i = 0; i < j; i++) {
+                if (max < prices[i] + revs[j - i - 1]) {
+                    max = prices[i] + revs[j - i - 1];
+                    size[j] = i + 1;
+                }
+            }
+            revs[j] = max;
+        }
+        
+        //For simplicity, return a 2d array where rs[0] is the revs array, rs[1] is the size array.
+        //This may not be the optimized solution but I think it's cumbersome to create a tuple class in Java so I choose to use a 2D array.
+        int[][] rs = new int[2][n + 1];
+        for (int i = 0; i < n + 1; i++) {
+            rs[0][i] = revs[i];
+            rs[1][i] = size[i];
+        }
+        return rs;
+    }
+
+    public void printCutRodSolution(int[] prices, int n) {
+        int[][] revsAndSize = extendedBottomUpCutRod(prices, n);
+        int maxRevenue = revsAndSize[0][n];
+        int[] size = revsAndSize[1];
+        while (n > 0) {
+            System.out.println(size[n]);
+            n -= size[n];
+        }
+        System.out.printf("max revenue: %d%n", maxRevenue);
     }
 }
